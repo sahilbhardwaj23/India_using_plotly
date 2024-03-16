f2284cf240e5b58adcc47060b5de3b6f6a708ab8
sahilbhardwaj23 <143410453+sahilbhardwaj23@users.noreply.github.com>
2024-03-16 16:37:55 +0530

    app.py

--- app.py
+++ app.py
@@ -8,34 +8,48 @@ st.set_page_config(layout='wide')
 df = pd.read_csv('india.csv')
 
 list_of_states = list(df['State'].unique())
-list_of_states.insert(0,'Overall India')
+list_of_states.insert(0, 'Overall India')
 
-st.sidebar.title('India Ka Data Viz')
+st.title("Interactive Data Visualization of Socio-Economic Indicators in India")
 
-selected_state = st.sidebar.selectbox('Select a state',list_of_states)
-primary = st.sidebar.selectbox('Select Primary Parameter',sorted(df.columns[5:]))
-secondary = st.sidebar.selectbox('Select Secondary Parameter',sorted(df.columns[5:]))
+st.write(
+    "Our project aims to provide an interactive platform for exploring key socio-economic indicators across different states of India."
+    " Leveraging the power of Plotly, a Python library for interactive data visualization, users can dynamically select specific states"
+    " or the entire country and compare various socio-economic parameters such as population, household with internet access, literacy rate, and sex ratio."
+    "The interface begins with a user-friendly selection menu allowing users to choose between individual states or the aggregated data for the entire country."
+    "Upon selection, users can then pick any two out of the three parameters: population, household with internet access, literacy rate, and sex ratio."
+    "Once the inputs are chosen, Plotly generates an interactive graph that dynamically visualizes the relationship between the selected parameters."
+    "Users can hover over data points to view detailed information and explore the trends and patterns across different states or regions."
+    "This project serves as a valuable tool for policymakers, researchers, and anyone interested in understanding the socio-economic landscape of India."
+    "By providing an intuitive interface and dynamic visualization capabilities, it facilitates deeper insights and informed decision-making regarding socio-economic "
+    "development initiatives."
+)
 
-plot = st.sidebar.button('Plot Graph')
+visualization_placeholder = st.empty()
 
-if plot:
+if st.sidebar.button('Go to Visualization'):
+    selected_state = st.sidebar.selectbox('Select a state', list_of_states)
+    primary = st.sidebar.selectbox('Select Primary Parameter', sorted(df.columns[5:]))
+    secondary = st.sidebar.selectbox('Select Secondary Parameter', sorted(df.columns[5:]))
 
-    st.text('Size represent primary parameter')
-    st.text('Color represents secondary parameter')
-    if selected_state == 'Overall India':
-        # plot for india
-        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,size_max=35,
-                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District')
+    plot = st.sidebar.button('Plot Graph')
 
-        st.plotly_chart(fig,use_container_width=True)
-    else:
-        # plot for state
-        state_df = df[df['State'] == selected_state]
+    if plot:
+        st.text('Size represent primary parameter')
+        st.text('Color represents secondary parameter')
+        if selected_state == 'Overall India':
+            # plot for india
+            fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,
+                                    size_max=35,
+                                    mapbox_style="carto-positron", width=1200, height=700, hover_name='District')
 
-        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=6, size_max=35,
-                                mapbox_style="carto-positron", width=1200, height=700,hover_name='District')
+            visualization_placeholder.plotly_chart(fig, use_container_width=True)
+        else:
+            # plot for state
+            state_df = df[df['State'] == selected_state]
 
-        st.plotly_chart(fig, use_container_width=True)
+            fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=6,
+                                    size_max=35,
+                                    mapbox_style="carto-positron", width=1200, height=700, hover_name='District')
 
-
-
+            visualization_placeholder.plotly_chart(fig, use_container_width=True)
